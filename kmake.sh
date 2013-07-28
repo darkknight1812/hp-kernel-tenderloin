cb07c2c28dea8d742cf166180be5ea2262bb9ec3
darkknight1812 <matt.gricius@gmail.com>
2013-07-28 01:17:17 -0700

    Update build script for linaro

--- kmake.sh
+++ kmake.sh
@@ -10,6 +10,9 @@ now=$(date +"%m_%d_%Y")
 
 #Build kernel using arm-eabi-4.4.3 toolchain
 #sudo make ARCH=arm -j16 CROSS_COMPILE=~/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- uImage
+
+#Build kernel using Linaro toolchain
+make ARCH=arm -j10 CROSS_COMPILE=arm-linux-gnueabi- uImage
 #echo "build complete"
 
 #Copy output to moboot folder
