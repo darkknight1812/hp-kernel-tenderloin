6222eecde294bf33251ff48e06685e17ae16c81d
darkknight1812 <matt.gricius@gmail.com>
2013-07-31 18:13:42 -0700

    Update scripts to new branch

--- kmake.sh
+++ kmake.sh
@@ -16,7 +16,7 @@ make ARCH=arm -j16 CROSS_COMPILE=arm-linux-gnueabi- uImage
 #echo "build complete"
 
 #Copy output to moboot folder
-cp ~/android/kernel/hp-kernel-tenderloin/arch/arm/boot/uImage ~/android/kernel/moboot/tools/uImage
+cp ~/android/kernel/uv/hp-kernel-tenderloin/arch/arm/boot/uImage ~/android/kernel/moboot/tools/uImage
 echo "Coppied"
 
 #Change directory to working directory
@@ -25,15 +25,15 @@ cd ~/android/kernel/moboot/tools
 #Make an uimage
 mkimage -A arm -O linux -T ramdisk -C none -a 0x60000000 -e 0x60000000 -n "Image" -d ramdisk.img uRamdisk
 echo "Ramdisk Complete"
-mkimage -A arm -O linux -T multi -a 0x40208000  -e  0x40208000 -C none -n "multi image" -d uImage:uRamdisk  uImage.CyanogenMod.new
+mkimage -A arm -O linux -T multi -a 0x40208000  -e  0x40208000 -C none -n "multi image" -d uImage:uRamdisk  uImage.CyanogenModuv.new
 echo "Uimage Complete"
 
 #Copy the uimage into boot.img
-cp -f uImage.CyanogenMod.new boot.img
+cp -f uImage.CyanogenModuv.new boot.img
 
 #Use the existing zip containter to create a flashable zip
-zip -f cm_kernel_tenderloin.zip boot.img
+zip -f cm_kernel_tenderloin_uv.zip boot.img
 
 #Create new build date
-cp cm_kernel_tenderloin.zip ~/Dropbox/cm_kernel_tenderloin_$now.zip
+cp cm_kernel_tenderloin_uv.zip ~/Dropbox/cm_kernel_tenderloin_uv_$now.zip
 echo "Finished"
