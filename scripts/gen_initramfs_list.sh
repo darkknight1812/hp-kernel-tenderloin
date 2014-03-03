bd9edaa600a3b8e0f3fdb34f755ca1f668761203
Daniel M. Weeks <dan@danweeks.net>
2014-03-03 15:38:28 -0800

    scripts/gen_initramfs_list.sh: fix flags for initramfs LZ4 compression
    LZ4 as implemented in the kernel differs from the default method now
    used by the reference implementation of LZ4.  Until the in-kernel method
    is updated to support the new default, passing the legacy flag (-l) to
    the compressor is necessary.  Without this flag the kernel-generated,
    LZ4-compressed initramfs is junk.
    
    Kyungsik said:
    
    : It seems that lz4 supports legacy format with the same option as lz4c
    : does.  Just looking at the first few bytes of lz4 compressed image, we can
    : see whether it is new format or not.
    :
    : It shows new format magic number without this patch.  New format magic
    : number is 0x184d2204.
    :
    : $ hexdump -C ./initramfs_data.cpio.lz4 |more
    : 00000000  04 22 4d 18 64 70 b9 69 (Little Endian)
    : ...
    :
    : Currently kernel supports legacy format only.
    
    Signed-off-by: Daniel M. Weeks <dan@danweeks.net>
    Cc: Michal Marek <mmarek@suse.cz>
    Acked-by: Kyungsik Lee <kyungsik.lee@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    	scripts/gen_initramfs_list.sh

--- scripts/gen_initramfs_list.sh
+++ scripts/gen_initramfs_list.sh
@@ -244,6 +244,24 @@ case "$arg" in
 		echo "$output_file" | grep -q "\.bz2$" && compr="bzip2 -9 -f"
 		echo "$output_file" | grep -q "\.lzma$" && compr="lzma -9 -f"
 		echo "$output_file" | grep -q "\.lzo$" && compr="lzop -9 -f"
+		echo "$output_file" | grep -q "\.gz$" \
+                && [ -x "`which gzip 2> /dev/null`" ] \
+                && compr="gzip -n -9 -f"
+		echo "$output_file" | grep -q "\.bz2$" \
+                && [ -x "`which bzip2 2> /dev/null`" ] \
+                && compr="bzip2 -9 -f"
+		echo "$output_file" | grep -q "\.lzma$" \
+                && [ -x "`which lzma 2> /dev/null`" ] \
+                && compr="lzma -9 -f"
+		echo "$output_file" | grep -q "\.xz$" \
+                && [ -x "`which xz 2> /dev/null`" ] \
+                && compr="xz --check=crc32 --lzma2=dict=1MiB"
+		echo "$output_file" | grep -q "\.lzo$" \
+                && [ -x "`which lzop 2> /dev/null`" ] \
+                && compr="lzop -9 -f"
+		echo "$output_file" | grep -q "\.lz4$" \
+                && [ -x "`which lz4 2> /dev/null`" ] \
+                && compr="lz4 -l -9 -f"
 		echo "$output_file" | grep -q "\.cpio$" && compr="cat"
 		shift
 		;;
