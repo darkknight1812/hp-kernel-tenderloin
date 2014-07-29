2a5b25d9931d12c6bd3a5bf60017fa7734d196c8
Darkknight1812 <matt.gricius@gmail.com>
2014-07-29 02:20:47 -0700

    Revise scripts for new branch

--- build_cm10.sh
+++ build_cm10.sh
@@ -2,7 +2,7 @@
 now=$(date +"%m_%d_%Y_%H_%M")
 
 #Copy output to moboot folder
-cp ~/android/kernel/cm10/hp-kernel-tenderloin/arch/arm/boot/uImage ~/android/kernel/moboot/tools/cm10/uImage
+cp ~/android/kernel/cm10v2/hp-kernel-tenderloin/arch/arm/boot/uImage ~/android/kernel/moboot/tools/cm10/uImage
 echo "Coppied"
 
 #Change directory to working directory
@@ -21,5 +21,5 @@ cp -f uImage.CyanogenMod.new boot.img
 zip -f cm10_kernel_tenderloin.zip boot.img
 
 #Create new build date
-cp cm10_kernel_tenderloin.zip ~/Dropbox/cm10_kernel_tenderloin_$now.zip
+cp cm10_kernel_tenderloin.zip ~/Dropbox/cm10_kernel_tenderloin_v2_$now.zip
 echo "Finished"
--- kmake_cm10.sh
+++ kmake_cm10.sh
@@ -16,7 +16,7 @@ make ARCH=arm -j16 CROSS_COMPILE=arm-linux-gnueabi- uImage
 #echo "build complete"
 
 #Copy output to moboot folder
-cp ~/android/kernel/cm10/hp-kernel-tenderloin/arch/arm/boot/uImage ~/android/kernel/moboot/tools/cm10/uImage
+cp ~/android/kernel/cm10v2/hp-kernel-tenderloin/arch/arm/boot/uImage ~/android/kernel/moboot/tools/cm10/uImage
 echo "Coppied"
 
 #Change directory to working directory
@@ -35,5 +35,5 @@ cp -f uImage.CyanogenMod.new boot.img
 zip -f cm10_kernel_tenderloin.zip boot.img
 
 #Create new build date
-cp cm10_kernel_tenderloin.zip ~/Dropbox/cm10_kernel_tenderloin_$now.zip
+cp cm10_kernel_tenderloin.zip ~/Dropbox/cm10_kernel_tenderloin_v2_$now.zip
 echo "Finished"
